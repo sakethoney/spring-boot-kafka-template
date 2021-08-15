c79c2b08d10a4817b597c783fb97b2d62b8e7575
sakethoney <saket.kumar.cs@gmail.com>
2021-08-15 16:18:42 -0500

    producer init

---
+++ placeholder-producer/src/main/java/com/saket/event/placeholder/producer/domain/LibraryEvent.java
@@ -0,0 +1,22 @@
+package com.saket.event.placeholder.producer.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LibraryEvent {
+
+	private Integer libraryEventId;
+	Book book;
+	public Integer getLibraryEventId() {
+		return libraryEventId;
+	}
+	public void setLibraryEventId(Integer libraryEventId) {
+		this.libraryEventId = libraryEventId;
+	}
+}
