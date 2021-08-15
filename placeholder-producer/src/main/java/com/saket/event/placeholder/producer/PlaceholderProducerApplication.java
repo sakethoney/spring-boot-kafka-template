c9bd99ab0d1a6238619621902593056e0f734012
sakethoney <saket.kumar.cs@gmail.com>
2021-08-15 16:15:47 -0500

    producer init

---
+++ placeholder-producer/src/main/java/com/saket/event/placeholder/producer/PlaceholderProducerApplication.java
@@ -0,0 +1,13 @@
+package com.saket.event.placeholder.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceholderProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceholderProducerApplication.class, args);
+	}
+
+}
